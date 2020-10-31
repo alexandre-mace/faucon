b6f5a1417a16f127e176b46fbb545e5dafba404a
alexandre-mace <contact.alexandremace@gmail.com>
2020-10-31 01:34:08 +0100

    V0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import data from './domain/definitions.json';
+
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
+}
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [definitions, setDefinitions] = useState([]);
+  const [currentDefinition, setCurrentDefinition] = useState([]);
+
+    useEffect(() => {
+        setDefinitions(data);
+    }, []);
+
+    useEffect(() => {
+        setCurrentDefinition(definitions.random())
+    }, [count, definitions]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <header>
+            <div id="maintitle">
+                Faucon
+            </div>
+            <div className={"count"}><span>{count}</span> désinformations comprises.</div>
+            <p id="subtitle">S'informer sur la désinformation</p>
+        </header>
+          {currentDefinition &&
+          <main>
+              <div>
+                  <div className={"definition-title"}>{currentDefinition.title}</div>
+                  <div className={"definition-source"}><a target="_blank" rel="noreferrer" href={currentDefinition.sourceUrl}>{currentDefinition.sourceName}</a></div>
+                  <div className={"definition-content"}>
+                      {currentDefinition.content}
+                  </div>
+              </div>
+              <button className={"action-button"} onClick={() => {
+                  setCount(count + 1);
+
+              } }>Compris</button>
+          </main>
+          }
+      </>
   );
 }
 
