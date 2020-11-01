acdd1ae1d8e85210ba4b2c1749c2f3b9765d3ba7
alexandre-mace <contact.alexandremace@gmail.com>
2020-11-01 11:26:58 +0100

    Rerandomize

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             ? definitions
             : definitions.filter(definition => definition !== currentDefinition.title)
 
-        getDefinition('Surcharge informationnelle', setCurrentDefinition)
+        getDefinition(potentialNewDefinitions.random(), setCurrentDefinition)
     }, [count, definitions]);
 
     return (
